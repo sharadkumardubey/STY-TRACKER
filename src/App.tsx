cb688c13e19fd56e090b9d50642a56d80439bdba
Sharad <35634504+sharadkumardubey@users.noreply.github.com>
2025-10-14 06:01:39 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <main className="flex flex-col items-center gap-8 py-16 max-w-[1280px] mx-auto"></main>
+  );
+}
+
+export default App;
